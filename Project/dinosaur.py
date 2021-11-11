25515728653dbf0266a50b6b2706a89a009c85ae
Tyler Pavlic <tmpavlic@gmail.com>
2021-11-11 10:21:09 -0600

    Dinosaur classes have been blueprinted.

--- Project/dinosaur.py
+++ Project/dinosaur.py
@@ -1,2 +1,10 @@
 class Dinosaurs:
-    pass
+    def __init__(self, name, attack_power):
+        self.name = name
+        self.ap = attack_power
+        self.health = "" #int
+        pass
+
+    def attack(self, robot): #void
+        pass
+        
--- Project/robot.py
+++ Project/robot.py
@@ -1,6 +1,9 @@
 class Robots:
     def __init__(self, name):
+        self.name = name
+        self.health = "" #int
+        self.weapon = "" #weapon
         pass
 
-    def attack(self, dinosaur):
+    def attack(self, dinosaur): #void
         pass
