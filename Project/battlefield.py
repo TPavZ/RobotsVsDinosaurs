0a050b79a8f95a12617b6834360462c47b8d05cd
Tyler Pavlic <tmpavlic@gmail.com>
2021-11-11 11:00:25 -0600

    battlefield class has been crewated, all class blueprints are now complete.

--- Project/battlefield.py
+++ Project/battlefield.py
@@ -1,2 +1,31 @@
+from fleet import Fleet
+from herd import Herd
+
 class Battlefield:
-    pass
+    def __init__(self):
+        self.fleet = Fleet
+        self.herd = Herd
+
+    def run_game(self): #void
+        pass
+
+    def display_welcome(self): #void
+        pass
+
+    def battle(self): #void
+        pass
+
+    def dino_turn(self, dinosaur): #void
+        pass
+
+    def robo_turn(self, robot): #void
+        pass
+
+    def show_doo(self): #void
+        pass
+
+    def show_roo(self): #void
+        pass
+
+    def display_winners(self): #void
+        pass
