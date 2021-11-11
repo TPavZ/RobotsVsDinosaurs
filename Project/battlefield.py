e312724d45d1ddffce666c9a5634db3d573bff87
Tyler Pavlic <tmpavlic@gmail.com>
2021-11-11 15:53:09 -0600

    Needed to collect my brain so I added my own drat.io UML.

--- Project/battlefield.py
+++ Project/battlefield.py
@@ -3,29 +3,29 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        self.fleet = Fleet()    
-        self.herd = Herd()
+        self.fleet = []   
+        self.herd = []
 
-    def run_game(self): #void
+    def run_game(self): #void ???
         pass
 
-    def display_welcome(self): #void
+    def display_welcome(self): #just a hello!
+        print("\n***WELCOME TO THE BATTLEFIELD***\n\n\t\tRobots\n\tVs.\n\t\tDinosaurs\n\n")
+
+    def battle(self): #void ???
         pass
 
-    def battle(self): #void
+    def dino_turn(self, dinosaur): #void Make the dino attack
         pass
 
-    def dino_turn(self, dinosaur): #void
+    def robo_turn(self, robot): #void Make the bot attack
         pass
 
-    def robo_turn(self, robot): #void
+    def show_doo(self): #void doo=dino opponent options ???
         pass
 
-    def show_doo(self): #void doo=dino opponent options
+    def show_roo(self): #void roo=robo opponent options ???
         pass
 
-    def show_roo(self): #void roo=robo opponent options
-        pass
-
-    def display_winners(self): #void
+    def display_winners(self): #void who wins and how?
         pass
